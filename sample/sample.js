16c267bc31fae9fb78bc55e5266eb1ce9a330460
Jiří Zajpt <jzajpt@blueberry.cz>
2012-02-19 22:34:53 +0100

    Fix ModalPane call

--- sample/sample.js
+++ sample/sample.js
@@ -3,7 +3,7 @@ SampleApp = Ember.Application.create();
 
 SampleApp.modalPaneController = Ember.Object.create({
   popup: function() {
-    Ember.ModalPane.popup({
+    Bootstrap.ModalPane.popup({
       heading: 'Sample modal pane',
       message: "Oh hell, how cool is this? It probably isn't.",
       primary: "Okey",
