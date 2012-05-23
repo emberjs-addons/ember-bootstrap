38b457dc4ba999c4acdc9defeaf73c111f1226ee
Igor Nemilentsev <polymetr@gmail.com>
2012-05-24 00:42:14 +0300

    Update Button class

--- packages/ember-bootstrap/lib/views/button.js
+++ packages/ember-bootstrap/lib/views/button.js
@@ -5,6 +5,17 @@ var get = Ember.get;
 
 Bootstrap.Button = Ember.Button.extend(Bootstrap.TypeSupport, Bootstrap.SizeSupport, {
   classNames: ['btn'],
-  classNameBindings: ['disabled'],
+  classNameBindings: ['typeClass', 'sizeClass', 'disabled'],
+
+  typeClass: Ember.computed(function() {
+    var type = get(this, 'type');
+    return type ? 'btn-' + type : null;
+  }).property('type').cacheable(),
+
+  sizeClass: Ember.computed(function() {
+    var size = get(this, 'size');
+    return size ? 'btn-' + size : null;
+  }).property('size').cacheable(),
+
   baseClassName: 'btn'
 });
