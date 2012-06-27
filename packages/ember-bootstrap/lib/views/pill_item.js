9acda3a852c17c8fa9ca275c917f59f153cecd6e
tony <oy@lianlianke.com>
2012-06-27 15:40:36 +0800

    use title of view instead of view of controller

--- packages/ember-bootstrap/lib/views/pill_item.js
+++ packages/ember-bootstrap/lib/views/pill_item.js
@@ -1,5 +1,5 @@
 require("ember-bootstrap/mixins/item_selection_support");
 
 Bootstrap.PillItem = Ember.View.extend(Bootstrap.ItemSelectionSupport, {
-  template: Ember.Handlebars.compile('<a href="#">{{title}}</a>')
+  template: Ember.Handlebars.compile('<a href="#">{{view.title}}</a>')
 });
