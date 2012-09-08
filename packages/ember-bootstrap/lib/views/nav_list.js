4858949529f5c472ed3a646b45385acbf0a4c92b
Damien Mathieu <42@dmathieu.com>
2012-09-08 13:25:03 +0200

    add support for href attribute in the nav list

--- packages/ember-bootstrap/lib/views/nav_list.js
+++ packages/ember-bootstrap/lib/views/nav_list.js
@@ -1,4 +1,5 @@
 require("ember-bootstrap/mixins/item_selection_support");
+require("ember-bootstrap/mixins/item_view_href_support");
 
 var get = Ember.get, set = Ember.set;
 var Bootstrap = window.Bootstrap;
@@ -7,7 +8,14 @@ Bootstrap.NavList = Ember.CollectionView.extend({
   classNames: ['nav', 'nav-list'],
   tagName: 'ul',
 
-  itemViewClass: Ember.View.extend(Bootstrap.ItemSelectionSupport, {
-    template: Ember.Handlebars.compile("<a href='#'>{{view.title}}</a>")
+  itemViewClass: Ember.View.extend(Bootstrap.ItemSelectionSupport, Bootstrap.ItemViewHrefSupport, {
+    template: Ember.Handlebars.compile('{{view view.item}}'),
+
+    item: Ember.View.extend({
+      tagName: 'a',
+      template: Ember.Handlebars.compile('{{view.parentView.title}}'),
+      attributeBindings: ['href'],
+      hrefBinding: 'parentView.href'
+    })
   })
 });
--- packages/ember-bootstrap/tests/views/nav_list_test.js
+++ packages/ember-bootstrap/tests/views/nav_list_test.js
@@ -62,4 +62,34 @@ test("a navigation list binds titles from custom property to DOM", function() {
   equal(jQuery(layer.find('li a')[1]).text(), 'Ohai', 'a nav list has a layer in the DOM');
 });
 
+test("a navigation list binds links from object to DOM", function() {
+  var layer;
+  navList = Bootstrap.NavList.create({
+    itemHrefKey: 'href',
+    content: new A([
+      { href: '/hello' },
+      { }
+    ])
+  });
+  appendIntoDOM(navList);
+  layer = navList.$();
+  equal(layer.find('li a').length, 2, 'a nav list has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).attr('href'), '/hello', 'a nav list has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).attr('href'), '#', 'a nav list has a layer in the DOM');
+});
 
+test("a navigation list binds titles from custom property to DOM", function() {
+  var layer;
+  navList = Bootstrap.NavList.create({
+    itemHrefKey: 'myLink',
+    content: new A([
+      { myLink: '/hello' },
+      { myLink: '/ohai' },
+    ])
+  });
+  appendIntoDOM(navList);
+  layer = navList.$();
+  equal(layer.find('li a').length, 2, 'a nav list has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).attr('href'), '/hello', 'a nav list has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).attr('href'), '/ohai', 'a nav list has a layer in the DOM');
+});
