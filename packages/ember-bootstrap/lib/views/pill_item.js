501a854bba71dbe3ee800d3a0294c953a7307d81
Sébastien Grosjean <public@zencocoon.com>
2013-04-16 13:30:06 +0300

    Make Tabs and Pills behave like NavList

--- packages/ember-bootstrap/lib/views/pill_item.js
+++ packages/ember-bootstrap/lib/views/pill_item.js
@@ -1,6 +1,14 @@
 require("ember-bootstrap/mixins/item_selection_support");
+require("ember-bootstrap/mixins/item_view_href_support");
 
 var Bootstrap = window.Bootstrap;
-Bootstrap.PillItem = Ember.View.extend(Bootstrap.ItemSelectionSupport, {
-  template: Ember.Handlebars.compile('<a href="#">{{view.title}}</a>')
+Bootstrap.PillItem = Ember.View.extend(Bootstrap.ItemSelectionSupport, Bootstrap.ItemViewHrefSupport, {
+  template: Ember.Handlebars.compile('{{view view.item}}'),
+
+  item: Ember.View.extend({
+    tagName: 'a',
+    template: Ember.Handlebars.compile('{{view.parentView.title}}'),
+    attributeBindings: ['href'],
+    hrefBinding: 'parentView.href'
+  })
 });
--- packages/ember-bootstrap/tests/views/pills_test.js
+++ packages/ember-bootstrap/tests/views/pills_test.js
@@ -1,4 +1,5 @@
 var get = Ember.get, set = Ember.set, A = Ember.A;
+var jQuery = window.jQuery;
 var application, pills;
 
 module("Bootstrap.Pills", {
@@ -25,3 +26,76 @@ test("pills can be stacked", function() {
   ok(pills.$().hasClass('nav-stacked'), 'an alert binds type property to class');
 });
 
+test("pills binds titles from content array to DOM", function() {
+  var layer;
+  pills = Bootstrap.Pills.create({
+    content: new A(['Hello', 'Ohai'])
+  });
+  appendIntoDOM(pills);
+  layer = pills.$();
+  equal(layer.find('li a').length, 2, 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).text(), 'Hello', 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).text(), 'Ohai', 'pills has a layer in the DOM');
+});
+
+test("pills list binds titles from object to DOM", function() {
+  var layer;
+  pills = Bootstrap.Pills.create({
+    content: new A([
+      { title: 'Hello' },
+      { title: 'Ohai' }
+    ])
+  });
+  appendIntoDOM(pills);
+  layer = pills.$();
+  equal(layer.find('li a').length, 2, 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).text(), 'Hello', 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).text(), 'Ohai', 'pills has a layer in the DOM');
+});
+
+test("pills binds titles from custom property to DOM", function() {
+  var layer;
+  pills = Bootstrap.Pills.create({
+    itemTitleKey: 'myKey',
+    content: new A([
+      { myKey: 'Hello' },
+      { myKey: 'Ohai' },
+    ])
+  });
+  appendIntoDOM(pills);
+  layer = pills.$();
+  equal(layer.find('li a').length, 2, 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).text(), 'Hello', 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).text(), 'Ohai', 'pills has a layer in the DOM');
+});
+
+test("pills binds links from object to DOM", function() {
+  var layer;
+  pills = Bootstrap.Pills.create({
+    content: new A([
+      { link: '/hello' },
+      { }
+    ])
+  });
+  appendIntoDOM(pills);
+  layer = pills.$();
+  equal(layer.find('li a').length, 2, 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).attr('href'), '/hello', 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).attr('href'), '#', 'pills has a layer in the DOM');
+});
+
+test("pills binds links from custom property to DOM", function() {
+  var layer;
+  pills = Bootstrap.Pills.create({
+    itemHrefKey: 'myLink',
+    content: new A([
+      { myLink: '/hello' },
+      { myLink: '/ohai' },
+    ])
+  });
+  appendIntoDOM(pills);
+  layer = pills.$();
+  equal(layer.find('li a').length, 2, 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[0]).attr('href'), '/hello', 'pills has a layer in the DOM');
+  equal(jQuery(layer.find('li a')[1]).attr('href'), '/ohai', 'pills has a layer in the DOM');
+});
