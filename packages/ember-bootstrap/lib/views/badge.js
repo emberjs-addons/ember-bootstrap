cc74bcb7289ddb24a792449d3bbd593b49fd4a95
Karl Guillotte <karl.guillotte.1@gmail.com>
2013-04-27 18:54:00 -0700

    Convert properties to Array and apply code styles
    There are some properties like classNames, attributesBindings and
    classNameBindings that should be Arrays. This commit converts them to
    Array.
    This project does not have code style guide, however I noticed a lot of
    double quotes mixed with single quotes. This commit converts some
    double quotes to single quotes. It will make the life easier for
    contributors.

--- packages/ember-bootstrap/lib/views/badge.js
+++ packages/ember-bootstrap/lib/views/badge.js
@@ -1,9 +1,9 @@
-require("ember-bootstrap/mixins/type_support");
+require('ember-bootstrap/mixins/type_support');
 
 var Bootstrap = window.Bootstrap;
 Bootstrap.Badge = Ember.View.extend(Bootstrap.TypeSupport, {
-  tagName: "span",
-  classNames: "badge",
-  baseClassName: "badge",
-  template: Ember.Handlebars.compile("{{view.content}}")
+  tagName: 'span',
+  classNames: ['badge'],
+  baseClassName: 'badge',
+  template: Ember.Handlebars.compile('{{view.content}}')
 });
--- packages/ember-bootstrap/lib/views/pager.js
+++ packages/ember-bootstrap/lib/views/pager.js
@@ -1,28 +1,28 @@
-require("ember-bootstrap/mixins/item_view_title_support");
-require("ember-bootstrap/mixins/item_view_href_support");
+require('ember-bootstrap/mixins/item_view_title_support');
+require('ember-bootstrap/mixins/item_view_href_support');
 
 var Bootstrap = window.Bootstrap;
 Bootstrap.Pager = Ember.CollectionView.extend({
-  tagName: "ul",
-  classNames: "pager",
-  itemTitleKey: "title",
-  itemHrefKey: "href",
+  tagName: 'ul',
+  classNames: ['pager'],
+  itemTitleKey: 'title',
+  itemHrefKey: 'href',
   init: function() {
     this._super();
-    if (!this.get("content")) {
-      this.set("content", Ember.A([
-                                  Ember.Object.create({ title: "&larr;" }), 
-                                  Ember.Object.create({ title: "&rarr;" })
+    if (!this.get('content')) {
+      this.set('content', Ember.A([
+                                  Ember.Object.create({ title: '&larr;' }), 
+                                  Ember.Object.create({ title: '&rarr;' })
       ]));
     }
   },
   itemViewClass: Ember.View.extend(Bootstrap.ItemViewTitleSupport, Bootstrap.ItemViewHrefSupport, {
-    classNameBindings: ["content.next", "content.previous", "content.disabled"],
+    classNameBindings: ['content.next', 'content.previous', 'content.disabled'],
     template: Ember.Handlebars.compile('<a {{bindAttr href="view.href"}}>{{{view.title}}}</a>')
   }),
   arrayDidChange: function(content, start, removed, added) {
     if (content) {
-      Ember.assert("content must always has at the most 2 elements", content.get("length") <= 2);
+      Ember.assert('content must always has at the most 2 elements', content.get('length') <= 2);
     }
     return this._super(content, start, removed, added);
   }
