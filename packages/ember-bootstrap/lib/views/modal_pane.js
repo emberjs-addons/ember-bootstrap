f0ac5c227426c239258c0bdd0c683b0c697c8554
boomsb <bradley.booms@gmail.com>
2013-08-22 10:39:41 -0500

    Update modal_pane.js
    Updated to work in Bootstrap 3.0.0

--- packages/ember-bootstrap/lib/views/modal_pane.js
+++ packages/ember-bootstrap/lib/views/modal_pane.js
@@ -1,63 +1,67 @@
 var get = Ember.get;
 
 var modalPaneTemplate = [
-'<div class="modal-header">',
-'  {{#if view.showCloseButton}}<a href="#" class="close" rel="close">&times;</a>{{/if}}',
-'  {{view view.headerViewClass}}',
-'</div>',
-'<div class="modal-body">{{view view.bodyViewClass}}</div>',
-'<div class="modal-footer">',
-'  {{view view.footerViewClass}}',
-'</div>'].join("\n");
+    '<div class="modal-dialog">',
+    '  <div class="modal-content">',
+    '    <div class="modal-header">',
+    '      {{#if view.showCloseButton}}<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>{{/if}}',
+    '      {{view view.headerViewClass}}',
+    '    </div>',
+    '    <div class="modal-body">{{view view.bodyViewClass}}</div>',
+    '    <div class="modal-footer">',
+    '      {{view view.footerViewClass}}',
+    '    </div>',
+    '  </div>',
+    '</div>' ].join("\n");
 
 var footerTemplate = [
-'{{#if view.parentView.secondary}}<a href="#" class="btn btn-secondary" rel="secondary">{{view.parentView.secondary}}</a>{{/if}}',
-'{{#if view.parentView.primary}}<a href="#" class="btn btn-primary" rel="primary">{{view.parentView.primary}}</a>{{/if}}'].join("\n");
-
-var modalPaneBackdrop = '<div class="modal-backdrop"></div>';
+    '{{#if view.parentView.secondary}}<button type="button" class="btn btn-default" rel="secondary">{{view.parentView.secondary}}</button>{{/if}}',
+    '{{#if view.parentView.primary}}<button type="button" class="btn btn-primary" rel="primary">{{view.parentView.primary}}</button>{{/if}}' ]
+    .join("\n");
 
 Bootstrap.ModalPane = Ember.View.extend(Ember.DeferredMixin, {
-  classNames: 'modal',
-  defaultTemplate: Ember.Handlebars.compile(modalPaneTemplate),
-  heading: null,
-  message: null,
-  primary: null,
-  secondary: null,
-  animateBackdropIn: null,
-  animateBackdropOut: null,
-  showBackdrop: true,
-  showCloseButton: true,
-  headerViewClass: Ember.View.extend({
-    tagName: 'h3',
-    template: Ember.Handlebars.compile('{{view.parentView.heading}}')
+  classNames : 'modal',
+  defaultTemplate : Ember.Handlebars.compile(modalPaneTemplate),
+  heading : null,
+  message : null,
+  primary : null,
+  secondary : null,
+  showBackdrop : true,
+  showCloseButton : true,
+  headerViewClass : Ember.View.extend({
+    tagName : 'h3',
+    template : Ember.Handlebars.compile('{{view.parentView.heading}}')
   }),
-  bodyViewClass: Ember.View.extend({
-    tagName: 'p',
-    template: Ember.Handlebars.compile('{{{view.parentView.message}}}')
+  bodyViewClass : Ember.View.extend({
+    tagName : 'p',
+    template : Ember.Handlebars.compile('{{{view.parentView.message}}}')
   }),
-  footerViewClass: Ember.View.extend({
-    template: Ember.Handlebars.compile(footerTemplate)
+  footerViewClass : Ember.View.extend({
+    template : Ember.Handlebars.compile(footerTemplate)
   }),
 
-  didInsertElement: function() {
-    if (get(this, 'showBackdrop')) this._appendBackdrop();
+  didInsertElement : function() {
+    this.$().modal({
+      backdrop : get(this, 'showBackdrop')
+    });
     this._setupDocumentKeyHandler();
   },
 
-  willDestroyElement: function() {
-    if (this._backdrop) this._removeBackdrop();
+  willDestroyElement : function() {
+    this.$().modal('hide');
     this._removeDocumentKeyHandler();
   },
 
-  keyPress: function(event) {
+  keyPress : function(event) {
     if (event.keyCode === 27) {
-      this._triggerCallbackAndDestroy({ close: true }, event);
+      this._triggerCallbackAndDestroy({
+        close : true
+      }, event);
     }
   },
 
-  click: function(event) {
-    var target = event.target,
-        targetRel = target.getAttribute('rel');
+  click : function(event) {
+    var target = event.target, targetRel = target.getAttribute('rel');
 
     if (targetRel) {
       var options = {};
@@ -68,44 +72,26 @@ Bootstrap.ModalPane = Ember.View.extend(Ember.DeferredMixin, {
     }
   },
 
-  _appendBackdrop: function() {
-    var parentLayer = this.$().parent(),
-        animateIn = this.get("animateBackdropIn");
-    this._backdrop = jQuery(modalPaneBackdrop).appendTo(parentLayer);
-    if (animateIn) this._backdrop.addClass("hide")[animateIn.method](animateIn.options);
-  },
-
-  _removeBackdrop: function() {
-    var animateOut = this.get("animateBackdropOut"),
-        _this = this;
-
-    if (animateOut) {
-      animateOut.options = jQuery.extend({always: function(){ _this._backdrop.remove();}}, animateOut.options);
-      this._backdrop[animateOut.method](animateOut.options);
-    } else {
-      this._backdrop.remove();
-    }
-  },
-
-  _setupDocumentKeyHandler: function() {
-    var cc = this,
-        handler = function(event) {
-          cc.keyPress(event);
-        };
+  _setupDocumentKeyHandler : function() {
+    var cc = this, handler = function(event) {
+      cc.keyPress(event);
+    };
     jQuery(window.document).bind('keyup', handler);
     this._keyUpHandler = handler;
   },
 
-  _removeDocumentKeyHandler: function() {
+  _removeDocumentKeyHandler : function() {
     jQuery(window.document).unbind('keyup', this._keyUpHandler);
   },
 
-  _resolveOrReject: function(options, event) {
-    if (options.primary) this.resolve(options, event);
-    else this.reject(options, event);
+  _resolveOrReject : function(options, event) {
+    if (options.primary || options.secondary)
+      this.resolve(options, event);
+    else
+      this.reject(options, event);
   },
 
-  _triggerCallbackAndDestroy: function(options, event) {
+  _triggerCallbackAndDestroy : function(options, event) {
     var destroy;
     if (this.callback) {
       destroy = this.callback(options, event);
@@ -118,14 +104,14 @@ Bootstrap.ModalPane = Ember.View.extend(Ember.DeferredMixin, {
 });
 
 Bootstrap.ModalPane.reopenClass({
-  rootElement: ".ember-application",
-  popup: function(options) {
+  rootElement : ".ember-application",
+  popup : function(options) {
     var modalPane, rootElement;
-    if (!options) options = {};
+    if (!options)
+      options = {};
     modalPane = this.create(options);
     rootElement = get(this, 'rootElement');
     modalPane.appendTo(rootElement);
     return modalPane;
   }
 });
-
