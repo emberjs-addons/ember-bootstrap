0f2b0621ccaf8e1203d7b1381ce3f1efe549c223
Damien Mathieu <42@dmathieu.com>
2013-09-02 17:57:04 +0200

    simplify the pager's default content

--- packages/ember-bootstrap/lib/views/pager.js
+++ packages/ember-bootstrap/lib/views/pager.js
@@ -6,19 +6,13 @@ Bootstrap.Pager = Ember.CollectionView.extend({
   classNames: ['pager'],
   itemTitleKey: 'title',
   itemHrefKey: 'href',
-  init: function() {
-    this._super();
-    if (!this.get('content')) {
-      this.set('content', Ember.A([
-                                  Ember.Object.create({ title: '&larr;' }), 
-                                  Ember.Object.create({ title: '&rarr;' })
-      ]));
-    }
-  },
+  content: Ember.A([Ember.Object.create({title: '&larr;'}), Ember.Object.create({title: '&rarr;'})]),
+
   itemViewClass: Ember.View.extend(Bootstrap.ItemViewTitleSupport, Bootstrap.ItemViewHrefSupport, {
     classNameBindings: ['content.next', 'content.previous', 'content.disabled'],
     template: Ember.Handlebars.compile('<a {{bindAttr href="view.href"}}>{{{view.title}}}</a>')
   }),
+
   arrayDidChange: function(content, start, removed, added) {
     if (content) {
       Ember.assert('content must always has at the most 2 elements', content.get('length') <= 2);
